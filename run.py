77cea0acf00ab3bd11848d924e0493aeac347c3d
Liu Shenghan <shl617@ucsd.edu>
2021-02-07 00:47:04 -0800

    add the user autophrase code

--- run.py
+++ run.py
@@ -19,17 +19,27 @@ data_config = json.load(open('config/data-params.json'))
 eda_config = json.load(open('config/eda-params.json'))
 
 def main(targets):
-    if ('sentiment' or 'all') in targets:
+    if ('data') in targets:
+        check_result_folder(**eda_config)
         reviews_list = split_data(data_config['outdir'])
-        df = sentiment.make_sentiment_table(reviews_list)
+        review = prepare_review_df(data_config['review'])
+    
+    if ('sentiment' or 'all') in targets:
+        df = make_sentiment_table(reviews_list)
         df, positive_phrases, negative_phrases = make_sentiment_table(reviews_list)
+        
     if ('eda' or 'all') in targets:
-        save_eda_data(df, positive_phrases, negative_phrases, eda_config['outdir'])
+        save_eda_data(df, positive_phrases, negative_phrases, eda_config['outdir'], eda_config['out_txt'], review)
         convert_eda(**eda_config)
+        
+    if ('clean') in targets:
+        clean_repo()
+        
     if 'test' in targets:
-        reviews_list = split_data('test/testdata/annotated_test.txt')
-        df, positive_phrases, negative_phrases = make_sentiment_table(reviews_list)
-        save_eda_data(df, positive_phrases, negative_phrases, eda_config['outdir'])
+        check_result_folder(**eda_config)
+        test_reviews_list, test_review = split_data(data_config['test_txt'], data_config['test_review'])
+        df, positive_phrases, negative_phrases = make_sentiment_table(test_reviews_list)
+        save_eda_data(df, positive_phrases, negative_phrases, eda_config['out_df'], eda_config['out_txt'], test_review)
         convert_eda(**eda_config)
         return
 
--- src/etl.py
+++ src/etl.py
@@ -1,7 +1,10 @@
+import os
+import numpy as np
+import shutil
 import pandas as pd
 import pdb
 
-def split_data(path):
+def split_data(path, path_review):
     '''Return txt back to rows, each row being a review'''
     with open(path, 'r') as f:
 #     with open('../../DSC180A-Project/data/in/yelp_reviews2.txt', 'r') as f:
@@ -10,4 +13,41 @@ def split_data(path):
 #         reviews_list = reviews_string.split("\n.\n")
         
     reviews_list = pd.Series(reviews_list)[:-1] # last entry is always empty
-    return reviews_list
+    dtypes = {
+        'review_id' : np.str,
+        'business_id': np.str,
+        'text': np.str,
+        'stars': np.float64,
+        'user_id': np.str
+        
+    }
+    return reviews_list , pd.read_csv(path_review, dtype = dtypes)
+
+def check_result_folder(out_df, out_img, out_txt, out_autophrase, **kwargs):
+    # create the result placement folder if does not exist
+    print(' --------Checking the reference folder now ------')
+    if os.path.isdir(out_df) is False:
+        command = 'mkdir -p ' + out_df
+        os.system(command)
+    if os.path.isdir(out_img) is False:
+        command = 'mkdir -p ' + out_img
+        os.system(command)
+    if os.path.isdir(out_txt) is False:
+        command = 'mkdir -p ' + out_txt
+        os.system(command)
+    if os.path.isdir(out_autophrase) is False:
+        command = 'mkdir -p ' + out_autophrase
+        os.system(command)
+    print(' --------The reference folder is ready ------')
+    return
+
+def clean_repo():
+    if os.path.isdir('reference') is True:
+        shutil.rmtree('reference')
+    if os.path.isdir('AutoPhrase/tmp') is True:
+        shutil.rmtree('AutoPhrase/tmp')
+    if os.path.isdir('tmp') is True:
+        shutil.rmtree('tmp')
+    if os.path.isdir('AutoPhrase/data/txt') is True:
+        shutil.rmtree('AutoPhrase/data/txt')
+    return
