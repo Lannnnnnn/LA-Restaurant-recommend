9ca3e39cee437359af5867d5f67bbba74ef3d287
catherinehou99 <catherinehou99@gmail.com>
2021-03-01 15:55:17 -0800

    add phoenix

--- test.py
+++ test.py
@@ -11,45 +11,40 @@ def find_substring(record, query):
 
 def test_function(text, location, foodbtn, vegan):
     lines = []
-    if location == "Las Vegas":
-        if foodbtn == 'ON':
-            df = pd.read_csv('./data/restaurant_phrases.csv')
-            if vegan == 'ON':
-                df = df[df['categories'].str.contains('vegan')]
-                
-            df['phrases'] = df['phrases'].apply(lambda x: json.loads(x))
-            
-            df = df.assign(numMentions=df['phrases']\
-            .apply(lambda x: find_substring(x, text)))\
-            .dropna()\
-            .sort_values(by='numMentions', ascending=False)
-            
-            df = df.head()
-            
-            df['stars'] = df['stars'].apply(lambda x: round(x, 1))
-            
-            df = df.assign(goodService=df['phrases']\
-            .apply(lambda x: find_substring(x, "service")))\
-            .sort_values(by='numMentions', ascending=False)    
-            
-            return df.values.tolist()
-            
-#             with open('test/testdata/lv_testdata.csv', 'r') as f:
-#                 info = f.readlines()[1:]
-#                 for i in info:
-#                     split_lines = i.replace('\n', '').replace('"', '').split(',')
-#                     lines.append(split_lines)
-#             pdb.set_trace()
-#             return lines
-        else:
-            rest = pd.read_csv('test/testdata/LV_rest_info.csv')
-            recomm_rest = []
-            target_column = ['name','address','stars','review_count','categories']
-            for i in range(5):
-                rank = 'Top_' + str(i)
-                target_id = rest[rest.name == text][rank].value_counts().idxmax()
-                recomm_rest.append(rest[rest.business_id == target_id][target_column].values.tolist()[0])
-            return recomm_rest
+    location = location.replace(' ', '_')
+    restaurant_path = './data/{0}.csv'.format(location)
+    if foodbtn == 'ON':
+#         df = pd.read_csv('./data/restaurant_phrases.csv')
+        df = pd.read_csv(
+        if vegan == 'ON':
+            df = df[df['categories'].str.contains('Vegetarian')]
+
+        df['phrases'] = df['phrases'].apply(lambda x: json.loads(x))
+
+        df = df.assign(numMentions=df['phrases']\
+        .apply(lambda x: find_substring(x, text)))\
+        .dropna()\
+        .sort_values(by='numMentions', ascending=False)
+
+        df = df.head()
+
+        df['stars'] = df['stars'].apply(lambda x: round(x, 1))
+
+        df = df.assign(goodService=df['phrases']\
+        .apply(lambda x: find_substring(x, "service")))\
+        .sort_values(by='numMentions', ascending=False)    
+
+        return df.values.tolist()
+
+    else:
+        rest = pd.read_csv('test/testdata/LV_rest_info.csv')
+        recomm_rest = []
+        target_column = ['name','address','stars','review_count','categories']
+        for i in range(5):
+            rank = 'Top_' + str(i)
+            target_id = rest[rest.name == text][rank].value_counts().idxmax()
+            recomm_rest.append(rest[rest.business_id == target_id][target_column].values.tolist()[0])
+        return recomm_rest
     else:
         with open('test/testdata/ph_testdata.csv', 'r') as f:
             info = f.readlines()[1:]
